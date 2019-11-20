2b0f535d1636585fc83fd1b55d19baaa0fd3c461
Luis Felipe Colosimo <luisfelipecaruzo@gmail.com>
2019-11-20 01:04:49 -0300

    aprendizado com rotas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,40 @@
 <?php
+Route::view('/view', 'welcome');
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+Route::get('/contato', function () {
+    return view('site.contato');
+});
+Route::get('users/{id}', function ($id) {
+    return "ola {$id}";
+});
 Route::get('/', function () {
     return view('welcome');
 });
+
+//qualquer tipo de req
+Route::any('/any', function () {
+    return 'any';
+});
+
+// falo que req quero
+Route::match(['get', 'post'], '/match', function () {
+    return 'match';
+});
+
+Route::get('users2/{id?}', function ($id = '') {
+    return "ola {$id}";
+});
+
+//redirecionar
+Route::get('red1', function () {
+    return redirect('/red2');
+});
+Route::redirect('red3', 'red2');
+
+Route::get('red4', function () {
+    return redirect()->route('url.name');
+});
+
+Route::get('red2', function () {
+    return "ola";
+})->name('url.name');
